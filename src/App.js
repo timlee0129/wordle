8b08a5e7d44d6ea64f535d2a1f82af609e18e2c0
timlee <timlee0129@gmail.com>
2022-11-07 18:12:49 -0800

    console log for invalid word

--- src/App.js
+++ src/App.js
@@ -8,99 +8,100 @@ import { boardDefault, ValidWordSet, AnswerWordSet } from './Words';
 export const AppContext = createContext();
 
 function App() {
-  const [board, setBoard] = useState(boardDefault);
-  const [currAttempt, setCurrAttempt] = useState({attempt: 0, letterPos: 0});
-  const [wordSet, setWordSet] = useState(new Set());
-  const [disabledLetters, setDisableLetter] = useState([]);
-  const [almostLetters, setAlmostLetter] = useState([]);
-  const [correctLetters, setCorrectLetter] = useState([]);
-  const [correctWord, setCorrectWord] = useState("")
-  const [gameOver, setGameOver] =useState({
-    gameOver: false,
-    guessedWord: false, 
-  })
-
-  useEffect(() => {
-    ValidWordSet().then((words) =>{
-      setWordSet(words.wordSet)
-    });
-    AnswerWordSet().then((words) => {
-      setCorrectWord(words.todaysWord)
-      //setCorrectWord("stoop")
+    const [board, setBoard] = useState(boardDefault);
+    const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
+    const [wordSet, setWordSet] = useState(new Set());
+    const [disabledLetters, setDisableLetter] = useState([]);
+    const [almostLetters, setAlmostLetter] = useState([]);
+    const [correctLetters, setCorrectLetter] = useState([]);
+    const [correctWord, setCorrectWord] = useState("")
+    const [gameOver, setGameOver] = useState({
+        gameOver: false,
+        guessedWord: false,
     })
-  }, []);
 
-  const onSelectLetter = (keyVal) => {
-    if (currAttempt.letterPos > 4) return;
-    const newBoard = [...board];
-    newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
-    setBoard(newBoard);
-    setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos+1});
-  }
+    useEffect(() => {
+        ValidWordSet().then((words) => {
+            setWordSet(words.wordSet)
+        });
+        AnswerWordSet().then((words) => {
+            setCorrectWord(words.todaysWord)
+            //setCorrectWord("stoop")
+        })
+    }, []);
 
-  const onDelete = () => {
-    if (currAttempt.letterPos === 0) return;
-    const newBoard = [...board];
-    newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
-    setBoard(newBoard);
-    setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos - 1});
-  }
-
-  const onEnter = () => {
-    if (currAttempt.letterPos !== 5) return;
-
-    let currWord = "";
-    for (let i=0; i<5; i++) {
-      currWord += board[currAttempt.attempt][i]
+    const onSelectLetter = (keyVal) => {
+        if (currAttempt.letterPos > 4) return;
+        const newBoard = [...board];
+        newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+        setBoard(newBoard);
+        setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos + 1 });
     }
 
-    if (wordSet.has(currWord.toLowerCase())){
-      setCurrAttempt({attempt: currAttempt.attempt+1, letterPos: 0});
-    } else {
-      alert("Invalid word");
-      return;
+    const onDelete = () => {
+        if (currAttempt.letterPos === 0) return;
+        const newBoard = [...board];
+        newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
+        setBoard(newBoard);
+        setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos - 1 });
     }
 
-    if (currWord === correctWord.toUpperCase()) {
-      setGameOver({gameOver: true, guessedWord: true});
-      return;
+    const onEnter = () => {
+        if (currAttempt.letterPos !== 5) return;
+
+        let currWord = "";
+        for (let i = 0; i < 5; i++) {
+            currWord += board[currAttempt.attempt][i]
+        }
+
+        if (wordSet.has(currWord.toLowerCase())) {
+            setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
+        } else {
+            console.log(wordSet, currWord.toLowerCase())
+            alert("Invalid word");
+            return;
+        }
+
+        if (currWord === correctWord.toUpperCase()) {
+            setGameOver({ gameOver: true, guessedWord: true });
+            return;
+        }
+
+        if (currAttempt.attempt === 5) {
+            setGameOver({ gameOver: true, guessedWord: false });
+        }
     }
 
-    if (currAttempt.attempt === 5) {
-      setGameOver({gameOver: true, guessedWord: false});
-    }
-  }
-
-  return (
-    <div className="App">
-      <nav>
-        <h1>Wordle</h1>
-      </nav>
-      <AppContext.Provider value={{ 
-        board, 
-        setBoard, 
-        currAttempt, 
-        setCurrAttempt, 
-        onSelectLetter, 
-        onDelete, 
-        onEnter,
-        correctWord,
-        disabledLetters, 
-        setDisableLetter,
-        gameOver, 
-        setGameOver,
-        almostLetters, 
-        setAlmostLetter,
-        correctLetters, 
-        setCorrectLetter,
-      }}>
-        <div className='game'>
-          <Board />
-          {gameOver.gameOver ? <GameOver />: <Keyboard />}
+    return (
+        <div className="App">
+            <nav>
+                <h1>Wordle</h1>
+            </nav>
+            <AppContext.Provider value={{
+                board,
+                setBoard,
+                currAttempt,
+                setCurrAttempt,
+                onSelectLetter,
+                onDelete,
+                onEnter,
+                correctWord,
+                disabledLetters,
+                setDisableLetter,
+                gameOver,
+                setGameOver,
+                almostLetters,
+                setAlmostLetter,
+                correctLetters,
+                setCorrectLetter,
+            }}>
+                <div className='game'>
+                    <Board />
+                    {gameOver.gameOver ? <GameOver /> : <Keyboard />}
+                </div>
+            </AppContext.Provider>
         </div>
-      </AppContext.Provider>
-    </div>
-  );
+    );
 }
 
 export default App;
